0d12fdf65ba8de534641f6e0457e99e7975f91d5
dtrongphuc <dtrongphucdv@gmail.com>
2020-07-07 16:03:25 +0700

    thêm framework caliburn micro

--- WeSplit/Views/HomeView.xaml.cs
+++ WeSplit/Views/HomeView.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WeSplit
+namespace WeSplit.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for HomeView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class HomeView : Window
     {
-        public MainWindow()
+        public HomeView()
         {
             InitializeComponent();
         }
