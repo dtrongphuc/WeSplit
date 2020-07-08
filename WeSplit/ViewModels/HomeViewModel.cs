ac3afc67061caaadf2acb8426456a5b443666631
dtrongphuc <dtrongphucdv@gmail.com>
2020-07-08 16:39:10 +0700

    test show to right

--- WeSplit/ViewModels/HomeViewModel.cs
+++ WeSplit/ViewModels/HomeViewModel.cs
@@ -4,10 +4,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace WeSplit.ViewModels
 {
-    class HomeViewModel:Screen
+    public class HomeViewModel:Screen
     {
+        private int _historywidth = 280;
+        public int HistoryWidth
+        {
+            get
+            {
+                return _historywidth;
+            }
+            set
+            {
+                _historywidth = value;
+                NotifyOfPropertyChange(() => HistoryWidth);
+            }
+        }
+     
+        public HomeViewModel()
+        {
+
+        }
+
+        public void ShowToRight()
+        {
+            HistoryWidth = 400;
+        }
     }
 }
