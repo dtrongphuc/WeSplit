70348b9a435e8a9da1d5580fd819f229b474439d
vy <vsbugvery@gmail.com>
2020-07-11 21:06:33 +0700

    models

---
+++ WeSplit/Models/Images.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeSplit.Models
+{
+    class Images : INotifyPropertyChanged
+    {
+        private string _TripID;
+        public string TripID
+        {
+            get { return _TripID; }
+            set
+            {
+                _TripID = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("TripID"));
+            }
+        }
+
+        private string _Image;
+        public string Image
+        {
+            get { return _Image; }
+            set
+            {
+                _Image = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Image"));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Images()
+        {
+            _TripID = "";
+            _Image = "Resource/Images/trip.jpg";
+        }
+
+        string sql;
+        public void Add()
+        {
+            sql = $"INSERT INTO HINHANH VALUES(MACD = {_TripID}, HINHANH='{_Image}')";
+            Connection.Execute_SQL(sql);
+        }
+
+        public void Edit()
+        {
+            sql = $"UPDATE HINHANH SET HINHANH='{_Image}' WHERE MACD = {_TripID}";
+            Connection.Execute_SQL(sql);
+        }
+
+        public void Delete()
+        {
+            sql = $"DELETE FROM HINHANH WHERE MACD = {_TripID} ";
+            Connection.Execute_SQL(sql);
+        }
+    }
+}
---
+++ WeSplit/Models/Member.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeSplit.Models
+{
+    class Member : INotifyPropertyChanged
+    {
+        private string _TripID;
+        public string TripID
+        {
+            get { return _TripID; }
+            set
+            {
+                _TripID = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("TripID"));
+            }
+        }
+
+        private string _MemberID;
+        public string MemberID
+        {
+            get { return _MemberID; }
+            set
+            {
+                _MemberID = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("MemberID"));
+            }
+        }
+
+        private string _MemberName;
+        public string MemberName
+        {
+            get { return _MemberName; }
+            set
+            {
+                _MemberName = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("MemberName"));
+            }
+        }
+
+        private string _Diary;
+        public string Diary
+        {
+            get { return _Diary; }
+            set
+            {
+                _Diary = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Diary"));
+            }
+        }
+
+        private int _Status;
+        public int Status
+        {
+            get { return _Status; }
+            set
+            {
+                _Status = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Status"));
+            }
+        }
+
+        private string _Telephone;
+        public string Telephone
+        {
+            get { return _Telephone; }
+            set
+            {
+                _Telephone = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Telephone"));
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Member()
+        {
+            this._TripID = "";
+            this._Telephone = "";
+            this._Status=0;
+            this._MemberName = "";
+            this._MemberID = "";
+            this._Diary = "";
+        }
+
+        string sql;
+        public void Add()
+        {
+             sql = $"INSERT INTO THANHVIEN VALUES (MATV ={_MemberID}, MACD = {_TripID}, HOTEN =N'{_MemberName}', NHATKY =N'{_Diary}', SDT='{_Telephone}', TRANGTHAI={_Status})";
+            Connection.Execute_SQL(sql);
+        }
+
+        public void Edit()
+        {
+             sql = $"UPDATE THANHVIEN  SET MACD = {_TripID}, HOTEN =N'{_MemberName}',  NHATKY =N'{_Diary}', SDT='{_Telephone}', TRANGTHAI={_Status} WHERE MATV ={_MemberID}";
+            Connection.Execute_SQL(sql);
+        }
+
+        public void Delete()
+        {
+            sql = $"DELETE FROM THANHVIEN WHERE MATV ={_MemberID} ";
+            Connection.Execute_SQL(sql);
+        }
+    }
+}
