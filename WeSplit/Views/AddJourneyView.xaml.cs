77871009692fde8bdf5db61c14cd851a36312631
nguyenhieunghia2001 <nghiadx2001@gmail.com>
2020-07-14 21:50:15 +0700

    no change

--- WeSplit/Views/AddJourneyView.xaml.cs
+++ WeSplit/Views/AddJourneyView.xaml.cs
@@ -225,5 +225,10 @@ namespace WeSplit.Views
 
             }//kết thúc thêm vào database
         }//két thúc sự kiện click
+
+        private List<CheckBox> AllChildrenCheckBox(object isLeader)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
